81eedc62929a0b4de4121c8e00a605ed25afe62c
Saugat Baraili <saugatbarailibk@gmail.com>
2022-08-11 15:17:31 +0545

    first commit

--- src/Components/ApplicationProcedures.jsx
+++ src/Components/ApplicationProcedures.jsx
@@ -15,6 +15,13 @@ export const ApplicationProcedures = (props) => {
     <div className="containerForPages">
     {/* <!-- start event block --> */}
 
+
+    <div className="Documentations">
+        <h2>Documenatation</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim quisquam esse soluta accusamus quidem. Laudantium iste aliquid asperiores unde quos eaque ducimus commodi error, ratione rem at laboriosam ab excepturi!</p>
+
+    </div>
+
 <div className="light_light">
     <div data-aos="fade-up-right" data-aos-anchor-placement="top-bottom" class="row align-items-center event-block no-gutters margin-40px-bottom">
         <div class="col-lg-7 order-2 order-lg-1 margin_autoP">
@@ -55,6 +62,46 @@ export const ApplicationProcedures = (props) => {
     </div>
 </div>
 
+
+<div className="light_light">
+    <div data-aos="fade-up-right" data-aos-anchor-placement="top-bottom" class="row align-items-center event-block no-gutters margin-40px-bottom">
+        <div class="col-lg-7 order-2 order-lg-1 margin_autoP">
+            <div class="padding-60px-lr cardZag_COntainer md-padding-50px-lr sm-padding-30px-all xs-padding-25px-all">
+                <h5 class="margin-15px-bottom md-margin-10px-bottom font-size22 md-font-size20 xs-font-size18 font-weight-500">
+                    Mock tests
+                    <hr />
+                </h5>
+                <p>Welcome to NIEC’s mocktest tests for TOEFL, IELTS, PTE, SAT 1, ACT, GRE and GMAT</p>
+            </div>
+        </div>
+        <div class="col-lg-5 order-1 order-lg-2">
+            <div class="position-relative ">
+                <img src="images/ben-mullins-oXV3bzR7jxI-unsplash.jpg" alt=""/>
+            </div>
+        </div>
+    </div>
+</div>
+
+    
+<div className="light_dark">
+    <div data-aos="fade-up-left" data-aos-anchor-placement="center-bottom" class="row align-items-center event-block no-gutters margin-40px-bottom">
+        <div class="col-lg-5 col-sm-12">
+            <div class="position-relative">
+                <img src="images/school-supplies-g91443fa5e_1280.jpg" alt=""/>
+            </div>
+        </div>
+        <div class="col-lg-7 col-sm-12">
+            <div class="padding-60px-lr cardZag_COntainer md-padding-50px-lr sm-padding-30px-all xs-padding-25px-all">
+                <h5 class="margin-15px-bottom md-margin-10px-bottom font-size22 md-font-size20 xs-font-size18 font-weight-500">
+                Provided Materials
+
+                    <hr />
+                </h5>
+                <p>Welcome to NIEC’s mocktest tests for TOEFL, IELTS, PTE, SAT 1, ACT, GRE and GMAT</p>
+            </div>
+        </div>
+    </div>
+</div>
     
 
     
--- src/Components/CountryCounselling.jsx
+++ src/Components/CountryCounselling.jsx
@@ -55,6 +55,48 @@ export const CountryCounselling = (props) => {
     </div>
 </div>
 
+
+<div className="light_light">
+    <div data-aos="fade-up-right" data-aos-anchor-placement="top-bottom" class="row align-items-center event-block no-gutters margin-40px-bottom">
+        <div class="col-lg-7 order-2 order-lg-1 margin_autoP">
+            <div class="padding-60px-lr cardZag_COntainer md-padding-50px-lr sm-padding-30px-all xs-padding-25px-all">
+                <h5 class="margin-15px-bottom md-margin-10px-bottom font-size22 md-font-size20 xs-font-size18 font-weight-500">
+                    Mock tests
+                    <hr />
+                </h5>
+                <p>Welcome to NIEC’s mocktest tests for TOEFL, IELTS, PTE, SAT 1, ACT, GRE and GMAT</p>
+            </div>
+        </div>
+        <div class="col-lg-5 order-1 order-lg-2">
+            <div class="position-relative ">
+                <img src="images/ben-mullins-oXV3bzR7jxI-unsplash.jpg" alt=""/>
+            </div>
+        </div>
+    </div>
+</div>
+
+    
+<div className="light_dark">
+    <div data-aos="fade-up-left" data-aos-anchor-placement="center-bottom" class="row align-items-center event-block no-gutters margin-40px-bottom">
+        <div class="col-lg-5 col-sm-12">
+            <div class="position-relative">
+                <img src="images/school-supplies-g91443fa5e_1280.jpg" alt=""/>
+            </div>
+        </div>
+        <div class="col-lg-7 col-sm-12">
+            <div class="padding-60px-lr cardZag_COntainer md-padding-50px-lr sm-padding-30px-all xs-padding-25px-all">
+                <h5 class="margin-15px-bottom md-margin-10px-bottom font-size22 md-font-size20 xs-font-size18 font-weight-500">
+                Provided Materials
+
+                    <hr />
+                </h5>
+                <p>Welcome to NIEC’s mocktest tests for TOEFL, IELTS, PTE, SAT 1, ACT, GRE and GMAT</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+
     
 
     
